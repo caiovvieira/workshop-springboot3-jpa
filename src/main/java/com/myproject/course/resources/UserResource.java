7c8f85db0c37cf2450ce05f8d836c21bdfba0929
Caio Vieira <caio@institutototum.com.br>
2024-11-19 15:03:12 -0300

    user entity and resource

---
+++ src/main/java/com/myproject/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.myproject.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myproject.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User user = new User(1L, "Caio", "caio@teste.com", "999999999", "12345");
+		return ResponseEntity.ok().body(user);
+	}
+	
+}
